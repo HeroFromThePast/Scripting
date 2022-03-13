a41e0a222cb3105f04ae8dbd2acacd714c47fcf7
Iuma <iumatoroyagari@gmail.com>
2022-03-13 18:24:53 -0500

    Level Manager

--- Assets/Scripts/Utility/LevelManager.cs
+++ Assets/Scripts/Utility/LevelManager.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    private int state = 0;
+    public Player player;
+    //public EnemyScript enemy;
+    public Tower tower;
+    public TowerCrafter towerCrafter;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //towerCrafter = GetComponent<TowerCrafter>();
+        GameObject towerObject = new GameObject();
+        towerCrafter = towerObject.AddComponent<TowerCrafter>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyScript enemy = new GameObject().AddComponent<EnemyScript>();
+        towerCrafter.CreateTower(enemy);
     }
+
+   
+   
 }
